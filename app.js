c8fca9652150e1b091c302046dc0ef770e9e40c7
tyoprataama <tyoprataama004@gmail.com>
2023-08-26 12:06:11 +0700

    refactor: change into routes function

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/simple.json`));
 
-//  READ
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     length: tours.length,
@@ -14,25 +13,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     },
   })
-})
+}
 
-//  CREATE
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({
-    id: newId
-  }, req.body);
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/data/simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'created',
-      data: newTour
-    })
-  })
-})
-
-//  READ
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
   // if (id > tours.length) 
@@ -48,10 +31,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   })
-})
+}
 
-//  UPDATE
-app.patch('/api/v1/tours/:id', (req, res) => {
+const postTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({
+    id: newId
+  }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/data/simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'created',
+      data: newTour
+    })
+  })
+}
+
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -64,10 +60,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated data>'
     }
   })
-})
+}
 
-//  DELETE
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -78,7 +73,24 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   })
-})
+}
+
+//  SAME WAY TO DEFINE HTTP METHOD FROM ROUTE FUNCTION BELOW
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', postTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+//  ALTERNATE
+app.route('/api/v1/tours')
+  .get(getAllTours)
+  .post(postTour);
+
+app.route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
