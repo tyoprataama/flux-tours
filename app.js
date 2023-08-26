18e725efca56b1c6665b964d559dfe98748eaa5d
tyoprataama <tyoprataama004@gmail.com>
2023-08-26 11:52:51 +0700

    feat: creating http method

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/simple.json`));
+
+//  READ
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -14,6 +16,7 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+//  CREATE
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({
@@ -28,6 +31,7 @@ app.post('/api/v1/tours', (req, res) => {
   })
 })
 
+//  READ
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
@@ -45,6 +49,37 @@ app.get('/api/v1/tours/:id', (req, res) => {
     }
   })
 })
+
+//  UPDATE
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'data not found'
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated data>'
+    }
+  })
+})
+
+//  DELETE
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'data not found'
+    })
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
