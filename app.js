04b2ff296e2a8e3a099437576975d62ae831aef1
tyoprataama <tyoprataama004@gmail.com>
2023-08-26 11:16:04 +0700

    feat: creating http method

--- app.js
+++ app.js
@@ -3,27 +3,47 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
-const tour = JSON.parse(fs.readFileSync(`${__dirname}/data/simple.json`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/simple.json`));
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    length: tour.length,
+    length: tours.length,
     data: {
-      tour
+      tours
     },
   })
 })
+
 app.post('/api/v1/tours', (req, res) => {
-  const newId = tour[tour.length - 1].id + 1;
-  const newTour = Object.assign({id: newId}, req.body);
-  tour.push(newTour);
-  fs.writeFile(`${__dirname}/data/simple.json`, JSON.stringify(tour), err => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({
+    id: newId
+  }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/data/simple.json`, JSON.stringify(tours), err => {
     res.status(201).json({
       status: 'created',
       data: newTour
     })
   })
+})
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  // if (id > tours.length) 
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'data not found'
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
 })
 const port = 3000;
 app.listen(port, () => {
