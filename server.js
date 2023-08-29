eceae7b6dc4e88f7fcc53451bd889cbd2d03f404
tyoprataama <tyoprataama004@gmail.com>
2023-08-29 23:44:11 +0700

    build: connecting DB

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './init.env' });
 const app = require('./app');
 
+const DB = process.env.DB.replace('<PASSWORD>', process.env.PASSWORD_DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection success!');
+  });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
