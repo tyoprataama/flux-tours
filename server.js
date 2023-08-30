6055e2d2c76490c8cfe8741b0ac11dbaef4d3116
tyoprataama <tyoprataama004@gmail.com>
2023-08-30 16:09:53 +0700

    build: testing document model

--- server.js
+++ server.js
@@ -15,6 +15,35 @@ mongoose
   .then(() => {
     console.log('DB connection success!');
   });
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const newTour = new Tour({
+  name: 'Your tour name',
+  rating: 4.9, // optional
+  price: 800
+});
+newTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('Error 👺', err);
+  });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
