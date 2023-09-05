2bf50eeb8317f6d870f9f4285f577272e4457683
tyoprataama <tyoprataama004@gmail.com>
2023-09-05 10:15:56 +0700

    build: handling unhandled routes

--- app.js
+++ app.js
@@ -20,5 +20,10 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
-
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannot found ${req.originalUrl} on this server!`
+  });
+});
 module.exports = app;
