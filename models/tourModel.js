0fd4fbe19dfac1832d4e03dbc7dae0fdb9255ef7
tyoprataama <tyoprataama004@gmail.com>
2023-10-05 22:58:12 +0700

    refactor: adding array type in tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -85,16 +85,18 @@ const tourSchema = new mongoose.Schema({
     address: String,
     description: String
   },
-  locations: {
-    type: {
-      type: String,
-      default: 'Point',
-      enum: ['Point']
-    },
-    coordinates: [Number],
-    address: String,
-    description: String
-  }
+  locations: [
+    {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    }
+  ]
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
