f7e6e4fd336b977745c9562a80c8275082fc7a30
tyoprataama <tyoprataama004@gmail.com>
2023-10-05 21:24:17 +0700

    build: locations on user schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -74,6 +74,26 @@ const tourSchema = new mongoose.Schema({
   secretTour: {
     type: Boolean,
     default: false
+  },
+  startPoint: {
+    type: {
+      type: String,
+      default: 'Point',
+      enum: ['Point']
+    },
+    coordinates: [Number],
+    address: String,
+    description: String
+  },
+  locations: {
+    type: {
+      type: String,
+      default: 'Point',
+      enum: ['Point']
+    },
+    coordinates: [Number],
+    address: String,
+    description: String
   }
 });
 const Tour = mongoose.model('Tour', tourSchema);
