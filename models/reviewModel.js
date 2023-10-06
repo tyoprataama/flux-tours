5697fc5ac48f1d75acb9acc87e19fd834021806c
tyoprataama <tyoprataama004@gmail.com>
2023-10-07 04:03:50 +0700

    build: populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -25,7 +25,16 @@ const reviewSchema = new mongoose.Schema({
     required: [true, 'Review must belong to a tour']
   }
 });
-
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+  next();
+});
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
