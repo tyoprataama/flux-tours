22fd5eb941426cabd2d38509f19b4848c9abd660
tyoprataama <tyoprataama004@gmail.com>
2023-10-12 15:22:55 +0700

    build: prevent user create more than 1 review on a same tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -40,6 +40,8 @@ reviewSchema.pre(/^find/, function(next) {
   next();
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.statics.calculateReviewAvg = async function(tourId) {
   const stats = await this.aggregate([
     { $match: { tour: tourId } },
