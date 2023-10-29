6ffa53030bf9b23f8f5543b8a974711157cfab00
tyoprataama <tyoprataama004@gmail.com>
2023-10-29 09:42:45 +0700

    build: mapbox sections

---
+++ public/js/mapbox.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmx1eHRvdXJzIiwiYSI6ImNsb2E5NWIxczAwZjQyam42M2dhemdyeTgifQ.jQAWiBw4Ze0G9drse_zKeA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  scrollZoom: false
+  // center: [112.60698908810181, -7.837910652942128],
+  // zoom: 7
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  // Add marker
+  new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+  // Add popup
+  new mapboxgl.Popup({
+      offset: 30
+    }).setLngLat(loc.coordinates)
+    .setHTML(`<p>${loc.address}</p>`)
+    .addTo(map);
+
+  // Extends map bounds to include current locations
+  bounds.extend(loc.coordinates)
+});
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 200,
+    left: 100,
+    right: 100
+  }
+})
